1835ff930e74110ce99e1c614cb96bce9527742a
Moisés <olimpiodev.tec@gmail.com>
2025-06-23 09:24:48 -0300

    Lendo temperatura da lista

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const temperaturas = [
+    {id: 1, temperatura: 22.5},
+    {id: 2, temperatura: 23.0},
+    {id: 3, temperatura: 21.8},
+    {id: 4, temperatura: 24.1},
+    {id: 5, temperatura: 22.0}
+]
+
 function simularLeitura() {
-    const temp = (Math.random() * 15 + 20).toFixed(1);
+    const numeroSorteado = Math.floor(Math.random() * temperaturas.length);
+    const sorteio = Math.floor(numeroSorteado);
+    const temp = temperaturas[sorteio].temperatura
     document.getElementById("temperature").textContent = `${temp} °C`;
 }
