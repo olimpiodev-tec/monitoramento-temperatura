7762f50361e44b1a285390437d24ecff0fca7d2c
Moisés <olimpiodev.tec@gmail.com>
2025-07-16 16:19:15 -0300

    update

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const clientId = 'Esp32' + Math.floor(Math.random() * 900) + 100;
 clienteWeb = new Paho.MQTT.Client('broker.hivemq.com', 8884, clientId);
 
 const temperaturaPagina = document.getElementById('temp')
+const umidadePagina = document.getElementById('umi')
 
 clienteWeb.onMessageArrived = function(message) {
     const payload = message.payloadString;
     const dados = JSON.parse(payload)
     
     temperaturaPagina.textContent = String(dados.temperatura) + ' °C'
+    umidadePagina.textContent = String(dados.umidade) + ' %'
     console.log('Dados: ' + payload)
 
     setTimeout(function(){
         temperaturaPagina.textContent = '- -'
+        umidadePagina.textContent = '- -'
     }, 2000)
     
 };
